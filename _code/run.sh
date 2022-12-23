3558aba04a313f6a38853acf71220601263760bf
Bobo <bnbsking@gmail.com>
2022-12-23 15:37:05 +0800

    Add files via upload

--- _code/run.sh
+++ _code/run.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 if [ $1 == "train" ]; then
-    python train.py --dataset cats_vs_dogs --data_path ./_data/mam/train --epochs 100 --eval_every 1
+    #python train.py --dataset cats_vs_dogs --data_path ./_data/888_v0/train --epochs 100 --eval_every 1
+    python train.py --dataset cats_vs_dogs --data_path ./_data/mam_v1/train --epochs 100 --eval_every 1
 elif [ $1 == "eval" ]; then
-    python eval.py --dataset cats_vs_dogs --data_path ./_data/mam/eval
+    #python eval.py --dataset cats_vs_dogs --data_path ./_data/888_v0/eval
+    #python eval.py --dataset cats_vs_dogs --data_path ./_data/mam_v0/eval
+    python eval.py --dataset cats_vs_dogs --data_path ./_data/mam_v0_unlabeled/test
 fi
