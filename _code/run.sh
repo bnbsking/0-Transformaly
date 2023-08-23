be8c9031edcfa1a1f00aa05250c6804040d907a6
Bobo <bnbsking@gmail.com>
2023-08-23 17:06:37 +0800

    Update run.sh

--- _code/run.sh
+++ _code/run.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-if [ $1 == "train" ]; then
-    #python train.py --dataset cats_vs_dogs --data_path ./_data/888_v0/train --epochs 100 --eval_every 1
-    python train.py --dataset cats_vs_dogs --data_path ./_data/mam_v1/train --epochs 100 --eval_every 1
-elif [ $1 == "eval" ]; then
-    #python eval.py --dataset cats_vs_dogs --data_path ./_data/888_v0/eval
-    #python eval.py --dataset cats_vs_dogs --data_path ./_data/mam_v0/eval
-    python eval.py --dataset cats_vs_dogs --data_path ./_data/mam_v0_unlabeled/test
+if [ $1 == "d1s1" ]; then
+    python train.py --dataset cats_vs_dogs --data_path ./_data/example/train --batch_size 4 --dataset_index d1 --epochs 10 --eval_every 1 
+elif [ $1 == "d1s2" ]; then
+    python eval.py  --dataset cats_vs_dogs --data_path ./_data/example/train --batch_size 4 --dataset_index d1
+elif [ $1 == "d2s1" ]; then
+    python train.py --dataset cats_vs_dogs --data_path ./_data/example/test  --batch_size 4 --dataset_index d2
+elif [ $1 == "d2s2" ]; then
+    python eval.py  --dataset cats_vs_dogs --data_path ./_data/example/test  --batch_size 4 --dataset_index d2
 fi
